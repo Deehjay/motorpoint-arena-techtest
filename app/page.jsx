b94a33c009e059923c0b45742db80fee51524e99
Deehjay <danielcjamesdj@gmail.com>
2023-03-10 12:22:23 +0000

    Add slideshow component to main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,12 @@
+import Slideshow from "./components/Carousel";
+import "./styles/Home.css";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="slideshow-wrapper">
+        <Slideshow />
+      </div>
+    </main>
+  );
 }
