169e07078735f4bcd00fd791ce7d1f8ca0e31118
Deehjay <danielcjamesdj@gmail.com>
2023-03-10 18:55:38 +0000

    Implement function to fetch events using axios

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,3 +1,16 @@
-export async function GET(request) {
-  return new Response('Hello, Next.js!')
+const axios = require("axios");
+
+const eventsApi = axios.create({
+  baseURL: "https://staging.national-ice-centre.com/api",
+});
+
+async function getEvents() {
+  const res = await eventsApi.get(
+    "/events/read?token=80d08db1-ed65-4bf0-a7ca-406febd5d917"
+  );
+  return res.data.events;
 }
+
+getEvents();
+
+module.exports = getEvents;
